ef468c627ee659e5f758daa6a836358a0aa47ce3
Merill Fernando <merill@merill.net>
2021-03-23 10:58:15 +1100

    Updated CA policies to split into files

--- src/Invoke-AADExporter.ps1
+++ src/Invoke-AADExporter.ps1
@@ -20,7 +20,7 @@ Function Invoke-AADExporter {
         [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
         [String]$Path,        
         [Parameter(Mandatory = $false)]
-        [ValidateSet('All', 'Config', 'Users')]
+        [ValidateSet('All', 'Config', 'ConditionalAccess', 'Users')]
         [String[]]$Type = 'Config',
         [Parameter(Mandatory = $false)]
         [object]$ExportSchema,
@@ -37,138 +37,139 @@ Function Invoke-AADExporter {
             @{
                 "Command" = "Get-AADExportUsers"
                 "Path" = "Users"
-                "Tag" = @("Users")
+                "Tag" = @("All", "Users")
                 "Childrens" = @(
                     @{
                         "Command" = "Get-AADExportAuthenticationMethodFIDO2"
                         "Path" = "Authentication/FIDO2Methods"
-                        "Tag" = @("Users")
+                        "Tag" = @("All", "Users")
                     },
                     @{
                         "Command" = "Get-AADExportAuthenticationMethodMicrosoftAuthenticator"
                         "Path" = "Authentication/MicrosoftAuthenticatorMethods"
-                        "Tag" = @("Users")
+                        "Tag" = @("All", "Users")
                     },
                     @{
                         "Command" = "Get-AADExportAuthenticationMethodWindowsHelloForBusiness"
                         "Path" = "Authentication/WindowsHelloForBusinessMethods"
-                        "Tag" = @("Users")
+                        "Tag" = @("All", "Users")
                     },
                     @{
                         "Command" = "Get-AADExportAuthenticationMethodTemporaryAccessPass"
                         "Path" = "Authentication/TemporaryAccessPassMethods"
-                        "Tag" = @("Users")
+                        "Tag" = @("All", "Users")
                     },
                     @{
                         "Command" = "Get-AADExportAuthenticationMethodPhone"
                         "Path" = "Authentication/PhoneMethods"
-                        "Tag" = @("Users")
+                        "Tag" = @("All", "Users")
                     },
                     @{
                         "Command" = "Get-AADExportAuthenticationMethodEmail"
                         "Path" = "Authentication/EmailMethods"
-                        "Tag" = @("Users")
+                        "Tag" = @("All", "Users")
                     },
                     @{
                         "Command" = "Get-AADExportAuthenticationMethodPassword"
                         "Path" = "Authentication/PasswordMethods"
-                        "Tag" = @("Users")
+                        "Tag" = @("All", "Users")
                     }
                 )
             },
             @{
                 "Command" = "Get-AADExportOrganization" 
                 "Path" = "Organization.json"
-                "Tag" = @("Config")
+                "Tag" = @("All", "Config")
             },
             @{
                 "Command" = "Get-AADExportSubscribedSkus"
                 "Path" = "SubscribedSkus.json"
-                "Tag" = @("Config")
+                "Tag" = @("All", "Config")
             },
             @{
                 "Command" = "Get-AADExportOrganizationBranding"
                 "Path" = "OrganizationBranding.json"
-                "Tag" = @("Config")
+                "Tag" = @("All", "Config")
             },
             @{
-                "Command" = "Get-AADExportConditionalAccessPolicies" 
-                "Path" =  "Identity/Conditional/AccessPolicies.json"
-                "Tag" = @("Config")
+                "Command" = "Get-AADExportConditionalAccessPolicies"
+                "Path" =  "Identity/Conditional/AccessPolicies"
+                "Tag" = @("All", "Config", "ConditionalAccess")
             },
-            #@{ ## 0817c655-a853-4d8f-9723-3a333b5b9235' is not an Azure AD B2C directory. Access to this Api can only be made for an Azure AD B2C directory.
-            #    "Command" = "Get-AADExportUserFlows"
-            #    "Path" = "Identity/UserFlows.json"
-            #},
+            @{
+                "Command" = "Get-AADExportUserFlows"
+                "Path" = "Identity/UserFlows.json"
+                "Tag" = @("B2C", "Config")
+            },
             @{
                 "Command" = "Get-AADExportDomains"
                 "Path" = "Domains.json"
-                "Tag" = @("Config")
+                "Tag" = @("All", "Config")
             },
             @{
                 "Command" = "Get-AADExportPoliciesIdentitySecurityDefaultsEnforcementPolicy"
                 "Path" =  "Policies/IdentitySecurityDefaultsEnforcementPolicy.json"
-                "Tag" = @("Config")
+                "Tag" = @("All", "Config")
             },
             @{
                 "Command" = "Get-AADExportPoliciesAuthorizationPolicy"
                 "Path" = "Policies/AuthorizationPolicy.json"
-                "Tag" = @("Config")
+                "Tag" = @("All", "Config")
             },
             @{
                 "Command" = "Get-AADExportIdentityProviders"
                 "Path" = "IdentityProviders.json"
-                "Tag" = @("Config")
+                "Tag" = @("All", "Config")
             },
             @{
                 "Command" = "Get-AADExportCertificateBasedAuthConfiguration"
                 "Path" = "Policies/CertificateBasedAuthConfiguration.json"
-                "Tag" = @("Config")
+                "Tag" = @("All", "Config")
             },
             @{
                 "Command" = "Get-AADExportCertificateBasedAuthConfiguration"
                 "Path" = "Policies/CertificateBasedAuthConfiguration.json"
-                "Tag" = @("Config")
+                "Tag" = @("All", "Config")
             },
             @{
                 "Command" = "Get-AADExportOrganizationSettings"
                 "Path" = "Organization/Settings.json"
-                "Tag" = @("Config")
+                "Tag" = @("All", "Config")
             },
             @{
                 "Command" = "Get-AADExportAuthenticationMethodPolicyEmail"
                 "Path" = "Policies/AuthenticationMethod/Email.json"
-                "Tag" = @("Config")
+                "Tag" = @("All", "Config")
             },
             @{
                 "Command" = "Get-AADExportAuthenticationMethodPolicyFIDO2"
                 "Path" = "Policies/AuthenticationMethod/FIDO2.json"
-                "Tag" = @("Config")
+                "Tag" = @("All", "Config")
             },
             @{
                 "Command" = "Get-AADExportAuthenticationMethodPolicyMicrosoftAuthenticator"
                 "Path" = "Policies/AuthenticationMethod/MicrosoftAuthenticator.json"
-                "Tag" = @("Config")
+                "Tag" = @("All", "Config")
             },
             @{
                 "Command" = "Get-AADExportAuthenticationMethodPolicySMS"
                 "Path" = "Policies/AuthenticationMethod/SMS.json"
-                "Tag" = @("Config")
+                "Tag" = @("All", "Config")
             },
             @{
                 "Command" =  "Get-AADExportAuthenticationMethodPolicyTemporaryAccessPass"
                 "Path" = "Policies/AuthenticationMethod/TemporaryAccessPass.json"
-                "Tag" = @("Config")
+                "Tag" = @("All", "Config")
             },
             @{
                 "Command" = "Get-AADExportPoliciesAdminConsentRequestPolicy"
                 "Path" = "Policies/AdminConsentRequestPolicy.json"
-                "Tag" = @("Config")
+                "Tag" = @("All", "Config")
             },
             @{
                 "Command" = "Get-AADExportIdentityGovernanceEntitlementManagementSettings"
                 "Path" = "IdentityGovernance/EntitlementManagement/Settings.json"
-                "Tag" = @("Config")
+                "Tag" = @("All", "Config")
             }
         )
     }
@@ -179,10 +180,11 @@ Function Invoke-AADExporter {
 
     foreach ($item in $ExportSchema) {
         $typeMatch = Compare-Object $item.Tag $Type -ExcludeDifferent
-        if($Type -contains 'All' -or $typeMatch) {
+        if( ($Type -contains 'All' -or $typeMatch)) {
             $outputFileName = Join-Path -Path $Path -ChildPath $item.Path
             $percentComplete = 100 * $processedItems / $totalExports
-            Write-Progress -Activity "Reading Azure AD Configuration" -CurrentOperation "Exporting $($item.Path)" -PercentComplete $percentComplete
+            Write-Host "Exporting $($item.Path)"
+            #Write-Progress -Activity "Reading Azure AD Configuration" -CurrentOperation "Exporting $($item.Path)" -PercentComplete $percentComplete
 
             $command = $item.Command
             if ($Parents){
