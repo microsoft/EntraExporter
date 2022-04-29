3962c2076b433c87bf1a445909257e0591d3b293
Sean McGrath <sean@mcgrath.net.nz>
2022-04-29 16:55:36 +1200

    Sort hashtables within child arrays

--- src/internal/ConvertTo-OrderedDictionary.ps1
+++ src/internal/ConvertTo-OrderedDictionary.ps1
@@ -21,7 +21,7 @@ function ConvertTo-OrderedDictionary
                 foreach ($Item in ($InputObject.GetEnumerator() | Sort-Object -Property Key)) {
                     if($Item){
                         $value = Get-ObjectProperty $Item 'Value'
-                        if($value -is [hashtable]){ #if child is a hashtable, sort it too
+                        if($value -is [hashtable] -or $value -is [array]){ #if child is a hashtable or array, sort it too
                             $Item.Value = ConvertTo-OrderedDictionary $value
                         }
                     }
